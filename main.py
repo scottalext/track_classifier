5eb6baeaa2f94dc1c916fc5ffd491b30e4927ab3
Alex Scott <scottalext@gmail.com>
2026-06-17 17:26:01 -0700

    refactoring into functions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import kagglehub
 import os
 import pandas as pd
+import requests
 
 
 
@@ -21,11 +22,26 @@ def generate_data_frame(data):
     df = pd.read_csv(data)
     return df
 
+def get_live_data():
+    session = requests.Session()
+    header = {
+        "Authorization": "Bearer " + os.getenv("SPOTIFY_CLIENT_SECRET")
+    }
+    search_url_base = "https://api.spotify.com/v1/search?"
+    search_query = "q=artist:ERRA"
+    combined_search_url = search_url_base + search_query
+
+    search_response = session.get(combined_search_url, headers=header, timeout=3)
+    print(search_response)
+
 def main():
+    """
+    Main program
+    """
     load_dotenv()
     historic_data = get_historic_data()
     historic_df = generate_data_frame(historic_data)
-    print(historic_df.head())
+    get_live_data()
 
 if "__main__" == __name__:
     main()
