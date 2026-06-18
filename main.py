8d219a97ab3b6d75ec1438ccc1928e158a01010f
Alex Scott <scottalext@gmail.com>
2026-06-17 17:00:24 -0700

    refactoring into functions

--- main.py
+++ main.py
@@ -3,12 +3,29 @@ import kagglehub
 import os
 import pandas as pd
 
-load_dotenv()
 
-# Historical data
-kaggle_key = os.getenv("KAGGLE_API_TOKEN")
 
-path = kagglehub.dataset_download("maharshipandya/-spotify-tracks-dataset") + "/dataset.csv"
 
-df_hist = pd.read_csv(path)
-print(df_hist.head())
+def get_historic_data():
+    """
+    Download Spotify Tracks data set from Kaggle
+    """
+    kaggle_key = os.getenv("KAGGLE_API_TOKEN")
+    path = kagglehub.dataset_download("maharshipandya/-spotify-tracks-dataset") + "/dataset.csv"
+    return path
+
+def generate_data_frame(data):
+    """
+    Generate pandas data frame for .csv data
+    """
+    df = pd.read_csv(data)
+    return df
+
+def main():
+    load_dotenv()
+    historic_data = get_historic_data()
+    historic_df = generate_data_frame(historic_data)
+    print(historic_df.head())
+
+if "__main__" == __name__:
+    main()
